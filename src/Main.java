4fcd6f39cebbd39d06e2c2aeffbfbaa3fabdc468
1 <Anasko162@Mail.ru>
2022-11-04 02:35:27 +0300

    Домашнее задание на Переменные часть 2

---
+++ src/Main.java
@@ -0,0 +1,109 @@
+public class Main {
+    public static void main(String[] args) {
+        int dog  = -11111111;
+        System.out.println ( " Значение переменной " +dog+ " с типом int Равно " +dog);
+        byte seasons = -122;
+        System.out.println ( " Значение переменной " +seasons+ " с типом byte Равно " +seasons);
+        short lessons = -22221;
+        System.out.println ( " Значение переменной " +lessons+ " с типом short Равно " +lessons);
+        long years = -22222222;
+        System.out.println ( " Значение переменной " +years+ " с типом long Равно " +years);
+        float boats  = 45.78f;
+        System.out.println ( " Значение переменной " +boats+ " с типом float Равно " +boats);
+        double bottles = 68.4207777777777 ;
+        System.out.println ( " Значение переменной " +bottles+ " с типом double Равно " +bottles);
+        double  bubbles = 27.12 ;
+        System.out.println ( " Значение переменной = " +bubbles);
+        long steps = 987678965549L ;
+        System.out.println ( " Значение переменной = " +steps);
+        double ways = 2.786;
+        System.out.println ( " Значение переменной = " +ways);
+        boolean b = 5 > 10;
+        System.out.println ( " Значение переменной = " +b);
+        short u = 569;
+        System.out.println ( " Значение переменной = " +u);
+        short i = -159;
+        System.out.println ( " Значение переменной = " +i);
+        short k = 27897;
+        System.out.println ( " Значение переменной = " +k);
+        byte j = 67;
+        System.out.println ( " Значение переменной = " +j);
+        float c = 23;
+        float v = 27;
+        float n = 30;
+        float l = 480;
+        float summa = c + v + n ;
+        float ListovBumagi = l / summa ;
+        System.out.println ( " На каждого ученика рассчитано " +ListovBumagi+ " листов бумаги ");
+        int x = 0 ; // вводится любое число продолжительности работы машины в минутах
+        int Butilok = 16;
+        int  Minut = 2;
+        int ButilokVminutu = Butilok / Minut ;
+        int Test = ButilokVminutu * 20;
+        System.out.println ( "За 20 минут машина произведет " +Test+ " бутылок ");
+        int TestTest1 = Test * 3;
+        System.out.println ( "За сутки машина произведет " +TestTest1+ " бутылок ");
+        int TestTest2 = TestTest1 * 3;
+        System.out.println ( "За 3 суток машина произведет " +TestTest2+ " бутылок ");
+        int TestTest3 = TestTest2 * 10 ;
+        System.out.println ( "За 30 суток(месяц) машина произведет " +TestTest3+ " бутылок ");
+        int BezPereriva = x * ButilokVminutu ;
+        System.out.println ( "За минут " + x + " машина произвела бутылок " +BezPereriva+ " штук" );
+        int Kraski = 120;
+        int KraskiNaclass =  2 + 4;
+        int KlassovVshkole = Kraski / KraskiNaclass ;
+        int BeloyKraski =  2 * 20 ;
+        int KorKraski = 4 * 20 ;
+        System.out.println ( " В школе, где " + KlassovVshkole + " классов, нужно  " +BeloyKraski+ " банок белой краски и " +KorKraski+ " банок коричневой краски " );
+        float DovaVka = 0;
+        float Banani = 80 ;
+        float Moloko =  105 ;
+        float Plombir = 100 ;
+        float Yaitca = 70 ;
+        float KolEdi1 = Banani * 5 ;
+        float KolEdi2 = Moloko * 2 ;
+        float KolEdi3 = Plombir *2 ;
+        float KolEdi4 = Yaitca * 4 ;
+        float SummaZavtraka = KolEdi1 + KolEdi2 + KolEdi3 + KolEdi4 ;
+        float VkiloGramm = SummaZavtraka / 1000 ;
+        System.out.println ( "Сумма веса завтрака " +VkiloGramm+ " кг "); // Непонятно, как сделать задачу с подсчетом новых случайных добавленных фруктов.
+        int Sportman  = 7000;
+        int Pot = 250;
+        int Pot2 = 500;
+        int KolDney1 = Sportman / Pot ;
+        int KolDney2 = Sportman / Pot2 ;
+        System.out.println ( " Дней на похудение по программе с потерей 250 грамм в день : " + KolDney1 );
+        System.out.println ( " Дней на похудение по программе с потерей 500 грамм в день : " + KolDney2 );
+        int DneyVsrednem = ( KolDney1 + KolDney2 ) /2;
+        System.out.println ( " Дней в среднем : " + DneyVsrednem );
+        double Masha = 67.760;
+        double Denis = 83.690;
+        double Kristina = 76.230;
+        double BolsheZarplati1 = 67.760 * 0.1 ;
+        double BolsheZarplati2 = 83.690 * 0.1 ;
+        double BolsheZarplati3 = 76.230 * 0.1;
+        double BolsheZarplati4 = BolsheZarplati1 + Masha;
+        double BolsheZarplati5 = BolsheZarplati2 + Denis;
+        double BolsheZarplati6 = BolsheZarplati3 + Kristina;
+        double BolsheZarplati7 = BolsheZarplati4 - Masha ;
+        double BolsheZarplati8 = BolsheZarplati5 - Denis;
+        double BolsheZarplati9 = BolsheZarplati6 - Kristina;
+        System.out.println ( " Маша теперь получает : "  + BolsheZarplati4 + " Годовой Доход вырос на : " + BolsheZarplati7 + " Рублей в месяц"  );
+        System.out.println ( " Денис теперь получает : "  + BolsheZarplati5 + " Годовой Доход вырос на : " + BolsheZarplati8 + " Рублей в месяц " );
+        System.out.println ( " Кристина теперь получает : "  + BolsheZarplati6 + " Годовой Доход вырос на : " + BolsheZarplati9 +  " Рублей в месяц" );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
